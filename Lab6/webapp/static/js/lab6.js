36d5f8bf589c49b273e18d9a4bbb524559a6af09
Santhosh Krishna <Santhosh Krishna>
2017-02-19 14:39:07 -0800

    updated Lab6

--- Lab6/webapp/static/js/lab6.js
+++ Lab6/webapp/static/js/lab6.js
@@ -3,17 +3,21 @@ $(document).ready(function() {
     iotSource.onmessage = function(e) {
       var double_quote_formatted_data = e.data.replace(/'/g, '"');
       parsed_data = JSON.parse(double_quote_formatted_data);
-      console.log(parsed_data);
+      // console.log(parsed_data);
       clearEnvTables();
       clearInertialTables();
 
       updateEnvironmentalTableData(parsed_data);
       updateInertialTableData(parsed_data);
+      updateEnvironChartData(parsed_data);
+      updateInertialChart(parsed_data);
   }
 
 //Glabal arrays to capture the table data
 var env_table_data = [];
 var imu_table_data = [];
+var env_chart_data = [];
+var ine_chart_data = [];
 
 //====================Environment table functions=============================
 function updateEnvironmentalTableData(json_obj) {
@@ -89,9 +93,70 @@ function clearInertialTables() {
   });
 }
 
+//======================Env Chart Data =========================================
+var env_chart = new Morris.Line({
+  element: 'env-chart',
+  data:[
+  ],
+  xkey: 'time',
+  ykeys:['h'],
+  labels:['%RH']
 });
 
-//======================Env Chart Data =========================================
-var env_chart = new Morris.line({
-  
-})
+//build the chart data
+function updateEnvironChartData (json_obj) {
+  env_chart_data.push(json_obj);
+  if (env_chart_data.length >16) {
+    env_chart_data.shift();
+  }
+  updateEnvironChart(env_chart_data);
+}
+
+//update the Chart
+function updateEnvironChart(data) {
+  var chart_data = [];
+  data.forEach(function(d) {
+    env_record = {
+      time: d['timestamp'],
+      h: d['environmental']['humidity'].value.toFixed(2)
+    };
+    chart_data.push(env_record);
+    // console.log(env_record);
+  });
+  // console.log(env_chart);
+  env_chart.setData(chart_data);
+}
+
+// =============Inertial Data Chart=============================================
+var inr_chart = new Morris.Line({
+  element: 'accel-chart',
+  data:[
+  ],
+  xkey: 'time',
+  ykeys:['x', 'y','z'],
+  labels:['Accel-X','Accel-Y','Accel-Z']
+});
+
+function updateInertialChart(json_obj) {
+  ine_chart_data.push(json_obj);
+
+  if(ine_chart_data.length > 16) {
+    ine_chart_data.shift();
+    }
+  updateInertialChartData(ine_chart_data);
+}
+
+function updateInertialChartData(data) {
+  var iner_chart_data = [];
+  data.forEach(function(d) {
+    ine_record = {
+      time : d['timestamp'],
+      x : d['inertial']['accelerometer']['x'].toFixed(2),
+      y : d['inertial']['accelerometer']['y'].toFixed(2),
+      z : d['inertial']['accelerometer']['z'].toFixed(2)
+    }
+    iner_chart_data.push(ine_record);
+  });
+  inr_chart.setData(iner_chart_data);
+}
+});
